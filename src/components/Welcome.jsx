e6aac2fc742f161a5a4e27564c23b0ea43744435
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-20 01:24:53 +0530

    Add renderText helper function.
    renderText helper function helps in splitting the text and render them character by character.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,5 +1,18 @@
 import React, { useRef } from 'react'
 
+const renderText = (text, className, baseWeight = 400) => {
+    return [...text].map((char, index) => (
+        <span
+            key={index}
+            className={className}
+            style={{
+                fontVariationSettings: `"wght" ${baseWeight}`,
+            }}
+        >
+            {char === " " ? "\u00A0" : char} 
+        </span>
+    ))
+}
 
 const Welcome = () => {
 
@@ -8,8 +21,20 @@ const Welcome = () => {
 
     return (
         <section id="welcome">
-            <p ref={titleRef}>Hey, I'm Saifi! Welcome to my</p>
-            <h1 ref={subtitleRef} className='mt-7'>Portfolio</h1>
+            <p ref={titleRef}>
+                {renderText(
+                    "Hey, I'm Saifi! Welcome to my",
+                    "text-3xl font-georama",
+                    100,
+                )}
+            </p>
+            <h1 ref={subtitleRef} className='mt-7'>
+                {renderText(
+                    "portfolio",
+                    "text-9xl italic font-georama",
+                    400,
+                )}
+            </h1>
 
             <div className='small-screen'>
                 <p>This Portfolio is designed for desktop/tabled screen only.</p>
