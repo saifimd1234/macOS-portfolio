a8aed933764db94ba432addd1df9ed85c3b24e6f
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-20 00:55:01 +0530

    Add dayjs library to display time.
    useState and useEffect to display second running in real time.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
 import { navIcons, navLinks } from '#constants'
-import React from 'react'
+
 
 
 const Navbar = () => {
+  const [currentTime, setCurrentTime] = useState(dayjs());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(dayjs());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
   return (
     <nav>
-        <div>
-            <img src="/images/logo.svg" alt="Logo" />
-            <p className='font-bold text-xl'>Saifi's Portfolio</p>
-             <ul>
-            {navLinks.map((item) => (
-                <li key={item.id}>
-                    <p>{item.name}</p>
-                </li>
-            ))}
+      <div>
+        <img src="/images/logo.svg" alt="Logo" />
+        <p className='font-bold text-xl'>Saifi's Portfolio</p>
+        <ul>
+          {navLinks.map((item) => (
+            <li key={item.id}>
+              <p>{item.name}</p>
+            </li>
+          ))}
         </ul>
-        </div>
+      </div>
 
-        <div>
-          <ul>
-            {navIcons.map(({ id, img }) => (
-              <li key={id}>
-                <img src={img} className="icon-hover" alt={`icon-${id}`} />
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+        <time>
+          {currentTime.format('ddd MMM DD, h:mm:ss A')}
+        </time>
+      </div>
     </nav>
   )
 }
