1210001287e1c9d2ccd3be04bfadb4e76ce94939
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-20 01:17:33 +0530

    Edit the welcome section.
    The text and ref that will be used.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useRef } from 'react'
+
 
 const Welcome = () => {
-  return (
-    <div>Welcome</div>
-  )
+
+    const titleRef = useRef(null);
+    const subtitleRef = useRef(null);
+
+    return (
+        <section id="welcome">
+            <p ref={titleRef}>Hey, I'm Saifi! Welcome to my</p>
+            <h1 ref={subtitleRef} className='mt-7'>Portfolio</h1>
+
+            <div className='small-screen'>
+                <p>This Portfolio is designed for desktop/tabled screen only.</p>
+            </div>
+        </section>
+    )
 }
 
 export default Welcome
