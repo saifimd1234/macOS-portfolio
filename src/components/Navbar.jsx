9b93ad1a123065b243990e749f2a32e62d04dd70
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2026-01-20 00:36:19 +0530

    Add the Navbar sections.
    Add respective css to the base.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,18 @@ const Navbar = () => {
     <nav>
         <div>
             <img src="/images/logo.svg" alt="Logo" />
+            <p className='font-bold text-xl'>Saifi's Portfolio</p>
+             <ul>
+            {[
+                {id: 1, name:"Portfolio"},
+                {id: 2, name:"About"},
+                {id: 3, name:"Contact"},
+            ].map((item) => (
+                <li key={item.id}>
+                    <p>{item.name}</p>
+                </li>
+            ))}
+        </ul>
         </div>
     </nav>
   )
